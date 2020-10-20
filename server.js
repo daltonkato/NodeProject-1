f98ef0ad5123e8efd7fb1cd3e58ea428bae7b349
Dalton Kato Gomes <daltonkato@gmail.com>
2020-10-20 20:09:22 -0300

    define mongodb, insertOne, find and create show.ejs

--- server.js
+++ server.js
@@ -1,40 +1,57 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const server = 'mongodb://localhost:27017'
-const database = 'myDatabase'
+const ServerPort = 3000
 
-
-mongoose.connect(`${server}/${database}`,
-   { useNewUrlParser: true, useUnifiedTopology: true }
-).then(() => {
-   console.log("Successfully connect to MongoDB.")
-})
-   .catch(err => {
-      console.error("Connection error", err)
-      process.exit()
-   })
-
-app.listen(3000, function() {
-
-    console.log('server running on port 3000')
-
+app.listen(ServerPort, function(){
+    console.log('Your node js server is running on PORT:',ServerPort)
 })
 
 app.get('/', (req, res) => {
 
     res.render('index.ejs')
 
-})
+});
 
 app.set('view engine', 'ejs')
-
 app.use(bodyParser.urlencoded({ extended: true}))
 
 
-app.post('/show', (req,res ) => {
 
-    console.log('req.body')
+const MongoClient = require('mongodb').MongoClient
+const ServerDB = 'mongodb://localhost:27017'
 
+MongoClient.connect(ServerDB, { useUnifiedTopology: true },
+    (err, client) => {
+    if (err) return console.log(err)
+
+    console.log ('Connect to DB - OK')
+
+    db = client.db('crud-nodejs')
+
+})
+
+app.get('/', (req, res) => {
+    const cursor = db.collection('data').find()
+})
+
+app.get('/show', (req, res) => {
+    db.collection('data').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', { data: results })
+    })
+
+})
+
+app.post('/show', (req, res) => {
+    db.collection('data').insertOne(req.body, (err, result) => {
+        if (err) return console.log(err)
+        
+        console.log('Data was inserted sucessfully in DB')
+        res.redirect('/show')
+        db.collection('data').find().toArray((err, results) => {
+            console.log(results)
+
+        })    
+    })
 })
